7d463255134ad0af01e58bf74723d2ae795ee083
pankaj kumar <pankajamsali@gmail.com>
2025-01-07 15:20:35 +0530

    feat: add ClipLoader component and Table component with customizable props

--- app/feedbacks/[id]/page.tsx
+++ app/feedbacks/[id]/page.tsx
@@ -1,52 +1,87 @@
 'use client';
+
 import { useEffect, useState } from 'react';
-import getProjectName from '@/app/actions/getProject';
 import { useParams } from 'next/navigation';
+import { CSVLink } from 'react-csv';
+import { ClipLoader } from 'react-spinners';
+import {
+  Search,
+  Download,
+  Star,
+  Trash2,
+  ChevronLeft,
+  ChevronRight,
+} from 'lucide-react';
+import getProjectName from '@/app/actions/getProject';
 import getFeedbacks from '@/app/actions/getFeedback';
 import { AISummary } from '@/app/actions/Ai';
 import { EmbededCode } from '@/components/embedCode';
 import { CodeBlock } from '@/components/ui/code-block';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+interface Feedback {
+  id: number;
+  name: string;
+  email: string;
+  createdAt: Date;
+  feedback: string;
+  rating: number;
+  projectid: number;
+}
 
 export default function Page() {
   const { id } = useParams();
   const [project, setProject] = useState<string | null>(null);
-  const [feedbacks, setFeedbacks] = useState<
-    | {
-        id: number;
-        name: string;
-        email: string;
-        createdAt: Date;
-        feedback: string;
-        rating: number;
-        projectid: number;
-      }[]
-    | null
-  >(null);
+  const [feedbacks, setFeedbacks] = useState<Feedback[] | null>(null);
+  const [filteredFeedbacks, setFilteredFeedbacks] = useState<Feedback[] | null>(
+    null
+  );
+  const [summary, setSummary] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState<'name' | 'rating' | 'date'>('date');
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isSummaryLoading, setIsSummaryLoading] = useState(false);
 
-  const [summary, setSummary] = useState<string>(' ');
+  const feedbacksPerPage = 4;
 
-  async function getSummary(
-    feedbacks:
-      | {
-          id: number;
-          name: string;
-          email: string;
-          createdAt: Date;
-          feedback: string;
-          rating: number;
-          projectid: number;
-        }[]
-      | null
-  ) {
+  async function getSummary(feedbacks: Feedback[] | null) {
     if (feedbacks) {
+      setIsSummaryLoading(true);
       if (feedbacks.length === 0) {
         setSummary('No feedbacks available');
-        return;
+      } else {
+        const summary = await AISummary(feedbacks);
+        if (summary) {
+          setSummary(summary);
+        }
       }
-      const summary = await AISummary(feedbacks);
-      if (summary) {
-        setSummary(summary);
-      }
+      setIsSummaryLoading(false);
     }
   }
 
@@ -61,110 +96,336 @@ export default function Page() {
         }
         if (Feedbacks !== undefined) {
           setFeedbacks(Feedbacks);
+          setFilteredFeedbacks(Feedbacks);
         }
+        setIsLoading(false);
       }
     }
     fetchProjectName();
   }, [id]);
 
+  useEffect(() => {
+    if (feedbacks) {
+      const filtered = feedbacks.filter(
+        (feedback) =>
+          feedback.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          feedback.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          feedback.feedback.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      const sorted = [...filtered].sort((a, b) => {
+        if (sortBy === 'name') return a.name.localeCompare(b.name);
+        if (sortBy === 'rating') return b.rating - a.rating;
+        return (
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+      });
+      setFilteredFeedbacks(sorted);
+      setCurrentPage(1);
+    }
+  }, [feedbacks, searchTerm, sortBy]);
+
+  const handleDelete = async (id: number) => {
+    setIsDeleting(true);
+    // Implement your delete logic here
+    // For example: await deleteFeedback(id);
+    setFeedbacks((prev) =>
+      prev ? prev.filter((feedback) => feedback.id !== id) : null
+    );
+    setIsDeleting(false);
+  };
+
+  const indexOfLastFeedback = currentPage * feedbacksPerPage;
+  const indexOfFirstFeedback = indexOfLastFeedback - feedbacksPerPage;
+  const currentFeedbacks = filteredFeedbacks?.slice(
+    indexOfFirstFeedback,
+    indexOfLastFeedback
+  );
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <ClipLoader color="#3B82F6" size={50} />
+      </div>
+    );
+  }
+
   if (!project) {
-    return <div>Loading...</div>;
+    return <div>Project not found</div>;
   }
 
   return (
-    <div className="max-w-4xl mx-auto px-6 py-12">
-      <header className="text-center mb-10">
-        <h1 className="sm:text-4xl text-3xl font-bold text-gray-800 dark:text-gray-200">
-          <span className="font-bold text-sky-400">
-            Feed
-            <span className="text-gray-800 dark:text-[#E7E9EC]">-Wall</span>
-          </span>{' '}
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <header className="text-center mb-8">
+        <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
+          <span className="text-sky-500">Feed</span>
+          <span className="text-gray-900 dark:text-gray-100">-Wall</span>{' '}
           Feedbacks
         </h1>
-        <p className="text-base sm:text-lg text-gray-600 dark:text-gray-400 mt-4">
-          This section contains feedbacks and the project script embedded for
-          review.
-        </p>
+        <div className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+          Review and manage customer feedbacks for {project}
+        </div>
       </header>
 
-      <div>
-        <h2>Customer Feedbacks</h2>
-        <div className="flex flex-col">
-          {feedbacks?.map((feedback, index) => (
-            <div
-              key={index}
-              className="bg-white shadow-md rounded-lg p-4 flex flex-col my-4"
+      <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 mb-6">
+        <h2 className="text-xl font-semibold mb-2">Customer Feedbacks</h2>
+        <div className="text-sm text-gray-600 dark:text-gray-400 mb-4">
+          📊 View, search, and manage feedback from your customers. Use the
+          tools below to filter and analyze the data.
+        </div>
+        <div className="flex flex-col sm:flex-row justify-between items-center mb-4 space-y-4 sm:space-y-0">
+          <div className="flex items-center w-full">
+            <Search className="w-5 h-5 text-gray-400 mr-2" />
+            <Input
+              type="text"
+              placeholder="Search feedbacks..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full sm:w-[95%] "
+            />
+          </div>
+          <div className="flex items-center space-x-4 w-full sm:w-auto justify-end">
+            <Select
+              value={sortBy}
+              onValueChange={(value) =>
+                setSortBy(value as 'name' | 'rating' | 'date')
+              }
             >
-              <div>
-                <div>
-                  {feedback.name} - {feedback.email} -{' '}
-                  {feedback.createdAt.toLocaleDateString()} - {feedback.rating}{' '}
-                  - {feedback.feedback}
-                </div>
+              <SelectTrigger className="w-[140px]">
+                <SelectValue placeholder="Sort by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="name">Sort by Name</SelectItem>
+                <SelectItem value="rating">Sort by Rating</SelectItem>
+                <SelectItem value="date">Sort by Date</SelectItem>
+              </SelectContent>
+            </Select>
+            <CSVLink
+              data={filteredFeedbacks || []}
+              filename={`${project}_feedbacks.csv`}
+              className="flex items-center px-3 py-4 border rounded-md border-neutral-200 bg-white text-sm ring-offset-white placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-neutral-800 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:placeholder:text-neutral-400 dark:focus:ring-neutral-300 active:text-red-700 active:bg-red-50 h-9 hover:bg-gray-100 dark:hover:bg-neutral-900 whitespace-nowrap"
+            >
+              <Download className="w-4 h-4 mr-2" />
+              Export CSV
+            </CSVLink>
+          </div>
+        </div>
+        {currentFeedbacks && currentFeedbacks.length > 0 ? (
+          <>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[150px]">Name</TableHead>
+                  <TableHead className="w-[200px]">Email</TableHead>
+                  <TableHead>Feedback</TableHead>
+                  <TableHead className="w-[100px]">Rating</TableHead>
+                  <TableHead className="w-[100px]">Date</TableHead>
+                  <TableHead className="w-[100px]">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {currentFeedbacks.map((feedback) => (
+                  <TableRow key={feedback.id}>
+                    <TableCell className="font-medium">
+                      {feedback.name}
+                    </TableCell>
+                    <TableCell>{feedback.email}</TableCell>
+                    <TableCell className="max-w-xs truncate">
+                      {feedback.feedback}
+                    </TableCell>
+                    <TableCell className="whitespace-nowrap">
+                      {[...Array(5)].map((_, i) => (
+                        <Star
+                          key={i}
+                          className={`w-4 h-4 inline-block ${
+                            i < feedback.rating
+                              ? 'text-yellow-400 fill-current'
+                              : 'text-gray-300'
+                          }`}
+                        />
+                      ))}
+                    </TableCell>
+                    <TableCell>
+                      {new Date(feedback.createdAt).toLocaleDateString()}
+                    </TableCell>
+                    <TableCell>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button variant="outline" size="sm">
+                            View
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle>{feedback.name}</DialogTitle>
+                            <DialogDescription>
+                              <span className="flex items-center space-x-2 my-2 pb-4">
+                                <span>{feedback.email}</span>
+                                <span className="flex items-center">
+                                  {[...Array(5)].map((_, i) => (
+                                    <Star
+                                      key={i}
+                                      className={`w-4 h-4 inline-block ${
+                                        i < feedback.rating
+                                          ? 'text-yellow-400 fill-current'
+                                          : 'text-gray-300'
+                                      }`}
+                                    />
+                                  ))}
+                                </span>
+                              </span>
+                              <div className="my-2 border-t border-gray-200 dark:border-gray-700"></div>
+                            </DialogDescription>
+                          </DialogHeader>
+                          <div className=" text-sm space-y-2">
+                            <div className="text-gray-700 dark:text-gray-300">
+                              {feedback.feedback}
+                            </div>
+                            <div className="text-gray-500 text-xs">
+                              Submitted:{' '}
+                              {new Date(feedback.createdAt).toLocaleString()}
+                            </div>
+                          </div>
+                          <div className="mt-4 flex justify-end">
+                            <Button
+                              variant="outline"
+                              onClick={() => handleDelete(feedback.id)}
+                              disabled={isDeleting}
+                              size="sm"
+                              className="text-red-600 hover:text-red-700 hover:bg-red-50"
+                            >
+                              {isDeleting ? (
+                                <ClipLoader color="#DC2626" size={16} />
+                              ) : (
+                                <>
+                                  <Trash2 className="w-4 h-4 mr-2" />
+                                  Delete
+                                </>
+                              )}
+                            </Button>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <div className="flex justify-between items-center mt-4">
+              <div className="text-sm text-gray-600 dark:text-gray-400">
+                Showing {indexOfFirstFeedback + 1} to{' '}
+                {Math.min(indexOfLastFeedback, filteredFeedbacks?.length || 0)}{' '}
+                of {filteredFeedbacks?.length} entries
+              </div>
+              <div className="flex space-x-2">
+                <Button
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                  size="sm"
+                >
+                  <ChevronLeft className="w-4 h-4" />
+                </Button>
+                <Button
+                  onClick={() => paginate(currentPage + 1)}
+                  disabled={
+                    indexOfLastFeedback >= (filteredFeedbacks?.length || 0)
+                  }
+                  size="sm"
+                >
+                  <ChevronRight className="w-4 h-4" />
+                </Button>
               </div>
             </div>
-          ))}
-        </div>
+          </>
+        ) : (
+          <div className="text-center py-8">
+            <div className="text-lg text-gray-600 dark:text-gray-400">
+              No feedbacks available 😢
+            </div>
+          </div>
+        )}
       </div>
-      <div>
-        <h2>Embed Code</h2>
-        <p>📋 Embed this code to add a feedback widget to your site!</p>
-        <div>
-          <EmbededCode
-            tab1Content={
-              <div>
-                <CodeBlock
-                  code={` <body>
-    <div style="position: fixed; bottom: 15px; right: 15px">
-      <widget-web-component theme="solarFlare" website-name=${project} projectid=${id}></widget-web-component>
-    </div>
-  
-    <script src="https://widget.opinify.in/widget.umd.js"></script>
-  </body>            `}
-                  language="html"
-                  filename="index.html"
-                />
-              </div>
-            }
-            tab2Content={
-              <div>
-                <CodeBlock
-                  code={` declare global {
-    namespace JSX {
-      interface IntrinsicElements {
-        "widget-web-component": React.DetailedHTMLProps<
-          React.HTMLAttributes<HTMLElement>,
-          HTMLElement
-        > & {
-          projectid: number;
-          theme: string;
-        };
-      }
+
+      <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 mb-6">
+        <h2 className="text-xl font-semibold mb-2">Embed Code</h2>
+        <div className="text-sm text-gray-600 dark:text-gray-400 mb-4">
+          🔗 Use this code to add the feedback widget to your website. Copy and
+          paste it into your HTML.
+        </div>
+        <EmbededCode
+          tab1Content={
+            <div>
+              <CodeBlock
+                code={`<body>
+  <div style="position: fixed; bottom: 15px; right: 15px">
+    <widget-web-component theme="solarFlare" website-name="${project}" projectid="${id}"></widget-web-component>
+  </div>
+
+  <script src="https://widget.opinify.in/widget.umd.js"></script>
+</body>`}
+                language="html"
+                filename="index.html"
+              />
+            </div>
+          }
+          tab2Content={
+            <div>
+              <CodeBlock
+                code={`declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "widget-web-component": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        projectid: number;
+        theme: string;
+      };
     }
   }
-  
-  <body>
-    <div style={{position: "fixed", bottom: "15px", right: "15px"}}>
-      <widget-web-component theme="solarFlare" website-name=${project} projectid=${id} />
-    </div>
+}
 
-    <script async src="https://widget.opinify.in/widget.umd.js"></script>
-  </body>`}
-                  language="typescript"
-                  filename="index.tsx"
-                />
-              </div>
-            }
-          />
-        </div>
+<body>
+  <div style={{position: "fixed", bottom: "15px", right: "15px"}}>
+    <widget-web-component theme="solarFlare" website-name="${project}" projectid="${id}" />
+  </div>
+
+  <script async src="https://widget.opinify.in/widget.umd.js"></script>
+</body>`}
+                language="typescript"
+                filename="index.tsx"
+              />
+            </div>
+          }
+        />
       </div>
-      <div>
-        <h2>Feedback Summary</h2>
-        <p>get summary through AI</p>
-        <button onClick={() => getSummary(feedbacks)}>Get summary</button>
-        <div>
-          <p>{summary}</p>
+
+      <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4">
+        <h2 className="text-xl font-semibold mb-2">Feedback Summary</h2>
+        <div className="text-sm text-gray-600 dark:text-gray-400 mb-4">
+          🤖 Get an AI-generated summary of your customer feedbacks to quickly
+          understand trends and insights.
         </div>
+        <Button
+          onClick={() => getSummary(feedbacks)}
+          className="mb-4"
+          disabled={isSummaryLoading}
+        >
+          {isSummaryLoading ? (
+            <>
+              <ClipLoader color="#FFFFFF" size={16} className="mr-2" />
+              Generating Summary...
+            </>
+          ) : (
+            'Get Summary'
+          )}
+        </Button>
+        {summary && (
+          <div className="border border-gray-200 dark:border-gray-700 p-4 rounded-md">
+            <div className="text-sm">{summary}</div>
+          </div>
+        )}
       </div>
     </div>
   );
