93946bd2c77d402902c8bed9b5274a188d5c46ef
pankaj kumar <pankajamsali@gmail.com>
2025-01-12 01:01:56 +0530

    feat: add logging for submitted feedback in the POST request handler

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -36,6 +36,7 @@ export async function POST(req: NextRequest) {
       },
     });
 
+    console.log(submittedFeedback);
     return NextResponse.json(
       {
         message: 'Feedback submitted successfully',
