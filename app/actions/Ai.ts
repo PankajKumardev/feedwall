3d27180cfc01946a9b8b797603435e15f7d16ab8
pankaj kumar <pankajamsali@gmail.com>
2025-09-27 22:27:00 +0530

    feat: update generative model to 'gemini-2.5-flash' for improved AI summary generation

--- app/actions/Ai.ts
+++ app/actions/Ai.ts
@@ -33,7 +33,7 @@ export const AISummary = async (
   console.log(feedbacks);
 
   const model = genAI.getGenerativeModel({
-    model: 'gemini-1.5-flash',
+    model: 'gemini-2.5-flash',
   });
   try {
     const result = await model.generateContent(prompt);
